300d5eef05580bf1433b69c4ee95361c6976c079
Marissa Gvozdenovich <66572562+mgvoz@users.noreply.github.com>
2025-03-12 17:16:11 -0400

    comment out personal finance out
    will uncomment when further along

--- src/data/workcards.ts
+++ src/data/workcards.ts
@@ -2,16 +2,16 @@ import constructionImage from "../assets/construction.jpg";
 import personalFinanceAppImage from "../assets/personal-finance-image.png";
 
 var workCards = [
-  {
-    title: "Personal Finance App",
-    image: personalFinanceAppImage,
-    description:
-      "An application that simulates scanning a user's email to find financial accounts and compiles the data into an interface. Planned improvements include enhanced filtering, more styling, additional charts & graphs, and functionality to cancel subscriptions. Upcoming code enhancements include adding robust comments and refactoring for optimization and conciseness. It is certainly still a work in progress.",
-    photoCredit: { link: "", text: "" },
-    link: "https://mgvoz.github.io/personal-finance-app/",
-    tags: ["React", "Bootstrap", "Typescript", "Github Pages", "ApexCharts"],
-    disableClick: false,
-  },
+  // {
+  //   title: "Personal Finance App",
+  //   image: personalFinanceAppImage,
+  //   description:
+  //     "An application that simulates scanning a user's email to find financial accounts and compiles the data into an interface. Planned improvements include enhanced filtering, more styling, additional charts & graphs, and functionality to cancel subscriptions. Upcoming code enhancements include adding robust comments and refactoring for optimization and conciseness. It is certainly still a work in progress.",
+  //   photoCredit: { link: "", text: "" },
+  //   link: "https://mgvoz.github.io/personal-finance-app/",
+  //   tags: ["React", "Bootstrap", "Typescript", "Github Pages", "ApexCharts"],
+  //   disableClick: false,
+  // },
   {
     title: "Coming Soon!",
     image: constructionImage,
